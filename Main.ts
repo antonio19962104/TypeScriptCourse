f4446983a69c3143c984cc0ace3881caf10f3648
jamurillo <jamurillo@grupoautofin.com>
2021-11-02 17:53:03 -0600

    rwer

--- Main.ts
+++ Main.ts
@@ -1,5 +1,7 @@
 class Main {
     run() {
+        let productList = new Array() : Products;
+
         let categoria = new Categorias(1, "Lacteos");
         let product = new Products(1, "Leche", categoria);
         console.log(product.getProductInfo());
